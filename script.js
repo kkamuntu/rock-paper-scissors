8a72924c02e6a44271002a1ecbb34543cd3d8791
Kirsten Kamuntu <268351006+kkamuntu@users.noreply.github.com>
2026-09-07 21:46:31 +0100

    add human choice function

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+        case 0:
+            return 'rock'
+        case 1:
+            return 'paper'
+        case 2:
+            return 'scissors'
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Rock, Paper or Scissors?")
+    return choice.toLowerCase()
+}
+console.log(getHumanChoice())
